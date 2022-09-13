577ffb7dae0c331b992539921e47e07d7b3d1d31
jack <842693026@qq.com>
2022-09-13 11:18:02 +0800

    feat：String类新增

--- gener-common/src/main/java/com/huanhai/utils/string/StringUtils.java
+++ gener-common/src/main/java/com/huanhai/utils/string/StringUtils.java
@@ -58,7 +58,7 @@ public class StringUtils {
      *null转空字符串
      *
      *@param str
-     *@return Boolean
+     *@return String
      *@author  qin bo
      *@date  2022-08-16
      *
@@ -67,6 +67,69 @@ public class StringUtils {
 
         return str == null ? EMPTY_STRING : str;
     }
+
+    /**
+     *字符串去掉前后空格
+     *
+     *@param str
+     *@return String
+     *@author  qin bo
+     *@date  2022-09-13
+     *
+     */
+    public static String trim(String str) {
+        return str == null ? null : str.trim();
+    }
+
+    /**
+     *字符串是否以某部分开头
+     *
+     *@param str
+     *@return Boolean
+     *@author  qin bo
+     *@date  2022-09-13
+     *
+     */
+    public static Boolean startWith(String str) {
+        return str == null ? Boolean.FALSE : str.startsWith(str);
+    }
+
+    /**
+     *字符串是否以某部分结尾
+     *
+     *@param str
+     *@return Boolean
+     *@author  qin bo
+     *@date  2022-09-13
+     *
+     */
+    public static Boolean endWith(String str) {
+        return str == null ? Boolean.FALSE : str.endsWith(str);
+    }
+
+    /**
+     *子串个数
+     *
+     *@param src
+     *@return Boolean
+     *@author  qin bo
+     *@date  2022-09-13
+     *
+     */
+    public static Integer subStringCount(String src,String rep) {
+        int result = 0;
+        if (isNullOrEmptyOfTrim(src) || isNullOrEmptyOfTrim(rep) ) {
+            return result;
+        }
+        int index= 0;
+        while ( (index= src.indexOf(rep,index)) > -1) {
+            result += 1;
+            index = index + rep.length();
+        }
+        return result;
+    }
+
+
 
 
 
